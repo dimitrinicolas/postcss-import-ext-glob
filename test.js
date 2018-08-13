936ac1ab84506c4ec5aaa7693baa9fa4546501c3
Dimitri Nicolas <dimitri.nicolas.cn@gmail.com>
2018-08-13 19:11:26 +0200

    Add ava-postcss-tester dev dependency

--- test.js
+++ test.js
@@ -1,117 +1,103 @@
 import test from 'ava';
+import PostcssTester from 'ava-postcss-tester';
 
 import postcss from 'postcss';
 import postcssImport from 'postcss-import';
 
 import postcssImportExtGlob from '.';
 
-const testPostcss = async (input, output, t, opts = {}) => {
-  const plugins = [];
-
-  for (const plugin of opts.pluginsBefore || []) {
-    plugins.push(plugin);
-  }
-  plugins.push(opts.plugin || postcssImportExtGlob());
-  for (const plugin of opts.pluginsAfter || []) {
-    plugins.push(plugin);
-  }
-
-  if (output instanceof Error) {
-    t.plan(1);
-  } else {
-    // t.plan(2);
-    t.plan(1);
-  }
-
-  await postcss(plugins).process(input, { from: '' }).then(result => {
-    if (output instanceof Error) {
-      t.fail();
-      return;
-    }
-    t.is(result.css.replace(/\n|\r/g, ' '), output.replace(/\n|\r/g, ' '));
-    // t.is(result.warnings().length, 0);
-  }).catch(err => {
-    if (!(output instanceof Error)) {
-      t.fail(err);
-      return;
-    }
-    t.is(err.message, output.message);
-  });
-};
+const tester = new PostcssTester({
+  postcss,
+  plugin: postcssImportExtGlob
+});
 
 test('simple test', async t => {
   const input = '@import-glob "fixtures/css/foo/**/*.css";';
-  const output = [
-    '.bar {',
-    '  display: inline-block;',
-    '}',
-    '.foo {',
-    '  display: block;',
-    '}'
-  ].join(' ');
-  await testPostcss(input, output, t, {
-    pluginsAfter: [postcssImport()]
+  const output = `
+    @import "${__dirname}/fixtures/css/foo/bar.css";
+    @import "${__dirname}/fixtures/css/foo/foo.css";
+  `;
+  await tester.test(input, output, t);
+});
+
+test('simple test postcss-import', async t => {
+  const input = '@import-glob "fixtures/css/foo/**/*.css";';
+  const output = `
+    .bar {
+      display: inline-block;
+    }
+    .foo {
+      display: block;
+    }
+  `;
+  await tester.test(input, output, t, {
+    pluginsAfter: [postcssImport]
   });
 });
 
 test('sort option', async t => {
   const input = '@import-glob "fixtures/css/foo/**/*.css";';
-  const output = [
-    '.foo {',
-    '  display: block;',
-    '}',
-    '.bar {',
-    '  display: inline-block;',
-    '}'
-  ].join(' ');
-  await testPostcss(input, output, t, {
-    plugin: postcssImportExtGlob({
+  const output = `
+    .foo {
+      display: block;
+    }
+    .bar {
+      display: inline-block;
+    }
+  `;
+  await tester.test(input, output, t, {
+    pluginOptions: {
       sort: 'desc'
-    }),
-    pluginsAfter: [postcssImport()]
+    },
+    pluginsAfter: [postcssImport]
   });
 });
 
 test('multiple globs', async t => {
-  const input = '@import-glob "fixtures/css/foo/**/*.css"; @import-glob "fixtures/css/*.css";';
-  const output = [
-    '.bar {',
-    '  display: inline-block;',
-    '}',
-    '.foo {',
-    '  display: block;',
-    '}',
-    'div {',
-    '  margin: auto;',
-    '}'
-  ].join(' ');
-  await testPostcss(input, output, t, {
-    pluginsAfter: [postcssImport()]
+  const input = `
+    @import-glob "fixtures/css/foo/**/*.css";
+    @import-glob "fixtures/css/*.css";
+  `;
+  const output = `
+    .bar {
+      display: inline-block;
+    }
+    .foo {
+      display: block;
+    }
+    div {
+      margin: auto;
+    }
+  `;
+  await tester.test(input, output, t, {
+    pluginsAfter: [postcssImport]
   });
 });
 
 test('multiple globs inline', async t => {
-  const input = '@import-glob "fixtures/css/foo/**/*.css",  "fixtures/css/*.css";';
-  const output = [
-    '.bar {',
-    '  display: inline-block;',
-    '}',
-    '.foo {',
-    '  display: block;',
-    '}',
-    'div {',
-    '  margin: auto;',
-    '}'
-  ].join(' ');
-  await testPostcss(input, output, t, {
-    pluginsAfter: [postcssImport()]
+  const input = `
+    @import-glob "fixtures/css/foo/**/*.css", "fixtures/css/*.css";
+  `;
+  const output = `
+    .bar {
+      display: inline-block;
+    }
+    .foo {
+      display: block;
+    }
+    div {
+      margin: auto;
+    }
+  `;
+  await tester.test(input, output, t, {
+    pluginsAfter: [postcssImport]
   });
 });
 
 test('error empty param test', async t => {
   const input = '@import-glob';
   const output = new Error('No string found with rule @import-glob ');
-  await testPostcss(input, output, t, {
-    pluginsAfter: [postcssImport()]
+  await tester.test(input, output, t, {
+    pluginsAfter: [postcssImport]
   });
 });
